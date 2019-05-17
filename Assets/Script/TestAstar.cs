2b159dea276b3d65da5f098f6d25d2d38a14cdce
qkqlgj8162 <qkqlgj8162@naver.com>
2019-05-17 13:53:20 +0900

    update

---
+++ Assets/Script/TestAstar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAstar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        print("hello world!!");
+    }   
+}
